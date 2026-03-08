577df33342fc5c192f9acd1e13a18596c544ed1c
ccwilliamson <cecelia.williamson22@gmail.com>
2026-03-08 10:07:44 -0400

    update: got gt table working

--- server.R
+++ server.R
@@ -1,8 +1,13 @@
 # load necessary packages
 # and source your functions.R file
-
 library(shiny)
 library(tidyverse)
+library(bslib)
+library(gt)
+library(dplyr)
+library(usmap)
+
+arrestByState <- read.csv("arrestByState.csv")
 
 source('functions.R')
 
@@ -10,5 +15,20 @@ source('functions.R')
 server <- function(input, output){
   # you will put your interactions here
   
+  
+  
+  output$myTable <- gt::render_gt({
+    
+    arrestByState %>%
+      gt() %>%
+      cols_label(state ~ "State", 
+                 murder ~ "Murder",
+                 assault ~ "Assault",
+                 urbanPop ~ "Urban Population",
+                 rape ~ "Rape") %>%
+      tab_header(title = "1973 Arrests per 100,000 People in Each State") %>%
+      tab_source_note(md(" **Source** McNeil, D. R. (1977) *Interactive Data Analysis*. New York: Wiley. ")) %>%
+      cols_align(align = "center", columns = everything())
+  })
   
 }
--- ui.R
+++ ui.R
@@ -1,10 +1,18 @@
 # Build your UI page here
 
 
-ui <- fluidPage(
-  # Fill in your details here
+ui <- bslib::page_fluid(
   
   
+  selectizeInput( 
+    "select", 
+    "Select options below:", 
+    list("Choice 1A" = "1A", "Choice 1B" = "1B", "Choice 1C" = "1C"), 
+    multiple = TRUE 
+  ), 
   
+  checkboxInput("checkbox", "Checkbox", FALSE), 
   
+  gt::gt_output("myTable")
+ 
 )
